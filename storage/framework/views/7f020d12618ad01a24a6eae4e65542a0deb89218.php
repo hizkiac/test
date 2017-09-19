23955b409fcf436aa65e4c2f7b5640cc46e97991
evan <try.evanggara@gmail.com>
2017-09-19 14:27:56 +0700

    commited index

--- storage/framework/views/7f020d12618ad01a24a6eae4e65542a0deb89218.php
+++ storage/framework/views/7f020d12618ad01a24a6eae4e65542a0deb89218.php
@@ -17,7 +17,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 
     <!-- Custom styles for this template -->
-    <link href="{{asset('css/album.css')}}" rel="stylesheet" type="text/css">
+    <link href="<?php echo e(asset('css/album.css')); ?>" rel="stylesheet" type="text/css">
   </head>
 
   <body>
